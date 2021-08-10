3d22056d4e0f43394a75c38e2350ca52b4ff3cc9
KStickney <31007720+KStickney@users.noreply.github.com>
2021-08-10 17:05:49 -0400

    Added Custom Classes from main.py

--- CustomClasses.py
+++ CustomClasses.py
@@ -2,6 +2,46 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 import sys
 
+class CapsValidator(QValidator):
+    def validate(self, string, pos):
+        return QValidator.Acceptable, string.upper(), pos
+
+class TabBar(QTabBar):
+    def tabSizeHint(self, index):
+        s = QTabBar.tabSizeHint(self, index)
+        s.transpose()
+        return s
+
+    def paintEvent(self, event):
+        painter = QStylePainter(self)
+        opt = QStyleOptionTab()
+
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            painter.drawControl(QStyle.CE_TabBarTabShape, opt)
+            painter.save()
+
+            s = opt.rect.size()
+            s.transpose()
+            r = QtCore.QRect(QtCore.QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
+
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            painter.drawControl(QStyle.CE_TabBarTabLabel, opt);
+            painter.restore()
+
+
+class TabWidget(QTabWidget):
+    def __init__(self, *args, **kwargs):
+        QTabWidget.__init__(self, *args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QTabWidget.West)
+        
+
 class CustomGridLayout(QVBoxLayout):
     def __init__(self):
         super(CustomGridLayout, self).__init__()
